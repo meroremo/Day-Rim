dd939424b62002036d51b71ce363682db5f453e1
meroremo <mero-yui@hotmail.de>
2016-04-29 22:32:30 +0200

    Characters Added + Dummy Character Switcher
    Change Character with 'S'
    TODO: Move Characters, Switch Characters with Touch Pad

---
+++ Day Rim/Assets/Scripts/SwitchCharacterController.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwitchCharacterController : MonoBehaviour {
+
+    public GameObject activeCharacter;
+    public GameObject felixGameObject;
+    public GameObject feliGameObject;
+
+    private PlayerCharacterController felix;
+    private PlayerCharacterController feli;
+
+	// Use this for initialization
+	void Start () 
+    {
+        felix = felixGameObject.GetComponent<PlayerCharacterController>();
+        feli = feliGameObject.GetComponent<PlayerCharacterController>();
+
+        setActiveCharacter();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if (Input.GetKeyUp(KeyCode.S))
+        {
+            felix.activeCharacter = !felix.activeCharacter;
+            feli.activeCharacter = !feli.activeCharacter;
+
+            setActiveCharacter();
+
+            Debug.Log(activeCharacter.gameObject.name);
+        }	
+    }
+
+    void setActiveCharacter()
+    {
+        if (feli.activeCharacter)
+        {
+            felix.activeCharacter = false;
+            activeCharacter = feliGameObject;
+        }
+        else if (felix.activeCharacter)
+        {
+            feli.activeCharacter = false;
+            activeCharacter = felixGameObject;
+        }
+    }
+}
