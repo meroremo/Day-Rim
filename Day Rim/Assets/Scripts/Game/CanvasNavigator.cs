c9a7127886d8f9b6e68d526fcf3d7aa6e575eb24
Vanessa <klappstuhlism@gmail.com>
2016-05-26 17:29:36 +0200

    Vanessa Zweiter Push
    Test Test

---
+++ Day Rim/Assets/Scripts/Game/CanvasNavigator.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CanvasNavigator : MonoBehaviour {
+
+    public static bool inventar_isActive = false;
+    private Animation inventar;
+    public GameObject Inventar_Panel;
+
+    // Use this for initialization
+    void Start () {
+        inventar = Inventar_Panel.GetComponent<Animation>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void OpenDialog()
+    {
+        if (!inventar_isActive)
+        {
+            inventar.Play("Inventar_IN");
+            inventar_isActive = true;
+            //hier in GameMode Inventar wechseln
+
+        }
+        else
+        {
+            inventar.Play("Inventar_OUT");
+            inventar_isActive = false;
+            //hier in GameMode INGame wechseln
+        }
+    }
+}
---
+++ Day Rim/Assets/Scripts/Game/DetectTouches.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class DetectTouches : MonoBehaviour {
+
+    private GameObject selectedObject;
+    private Ray ray;
+    private RaycastHit rayHit;
+
+	// Use this for initialization
+	void Start () {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            GameObject.Find("TestText").GetComponent<Text>().text = "HOT HOT HOT";
+            ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+            Touch touch = Input.touches[0];
+            
+            // touchPosition = Input.GetTouch(0).position;
+
+            //if (!EventSystem.current.IsPointerOverGameObject(touch.fingerId))
+            //{
+            //    GameObject.Find("TestText").GetComponent<Text>().text = "CHICK CHICK CHICK";
+
+            if (Physics.Raycast(ray, out rayHit, Mathf.Infinity))
+                {
+                    GameObject.Find("TestText").GetComponent<Text>().text = "HIT HIT HIT";
+
+                    GameObject hitObject = rayHit.transform.gameObject;
+                    selectedObject = hitObject;
+
+                   
+
+                    if (hitObject.tag == "OpenDialog")
+                    {
+                        GameObject.Find("TestText").GetComponent<Text>().text = "LUISE LUISE LUISE";
+                        SceneManager.LoadScene("DialogScene");
+                    }
+
+
+
+                    /*        // GameObject.Find("TestText").GetComponent<Text>().text = "HIT HIT HIT";
+                            hit = Physics.Raycast(Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position), Vector2.zero);
+                    //GameObject.Find("TestText").GetComponent<Text>().text = Input.GetTouch(0).position.ToString();
+                   if (hit.collider != null && hit.transform.gameObject.tag == "Karton_Luise") 
+                    { 
+                        GameObject.Find("TestText").GetComponent<Text>().text = "Invenape getouched doooown..";
+                    }*/
+              //  }
+            }
+        }
+    }
+}
